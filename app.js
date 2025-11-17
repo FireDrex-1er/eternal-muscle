8cf1b30f64a301c2b20354e3f1fd8ce348541d93
Zehrion <timothefortier64@gmail.com>
2025-11-17 15:47:35 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ setInterval(() => {
     if (document.body.clientHeight > document.body.clientWidth ) {
         document.getElementById("Bras_Epaule").style.fontSize="20px";
         document.getElementById("Dos").style.fontSize="20px";
+        document.getElementById("Os").style.fontSize="20px";
     }
     if (document.body.clientHeight < document.body.clientWidth ) {
         document.getElementById("Dos").style.fontSize="30px";
         document.getElementById("Bras_Epaule").style.fontSize="30px";
+        document.getElementById("os").style.fontSize="30px";
     }
 }, 500);
