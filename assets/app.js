5b511515816706deb26f144e1c241880888d3ffd
Zehrion <timothefortier64@gmail.com>
2026-03-10 13:08:44 +0100

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -39,25 +39,25 @@ if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/T%C3%AAte_lon
     document.getElementById("body_1_titre").href="https://www.youtube.com/shorts/Oi3bW9nQmGI";
     document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
     document.getElementById("body_1_serie").innerHTML="4 séries";
-    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
-    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
-    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_repos").innerHTML="2min de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 2 secondes<br>Contraction en haut : 1 seconde<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="";
     document.getElementById("body_1_%MVC").innerHTML="70% MVC(contraction maximale volontaire)";
 
     
-}if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/Brachial/") {
+}if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/brachioradial/") {
     
     document.getElementById("body_0").style.top="-100%";
     document.getElementById("body_1").style.top="0%";
 
-    document.getElementById("body_1_titre").innerHTML="Tractions prise serrée (chin-up)";
+    document.getElementById("body_1_titre").innerHTML="Reverse curl";
     document.getElementById("body_1_titre").href="https://www.youtube.com/shorts/Oi3bW9nQmGI";
     document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
     document.getElementById("body_1_serie").innerHTML="4 séries";
-    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
-    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
-    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
-    document.getElementById("body_1_%MVC").innerHTML="90 % MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_repos").innerHTML="1min30 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 2 secondes<br>Contraction en haut : 1 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="70 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="80 % MVC(contraction maximale volontaire)";
     
 }
 
