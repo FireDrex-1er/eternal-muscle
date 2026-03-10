227654dc763d24d0d6df2124c41cbcad807174f6
Zehrion <timothefortier64@gmail.com>
2026-03-10 13:22:31 +0100

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -5,14 +5,14 @@ if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/T%C3%AAte_lon
     document.getElementById("body_0").style.top="-100%";
     document.getElementById("body_1").style.top="0%";
 
-    document.getElementById("body_1_titre").innerHTML="Curl concentration";
-    document.getElementById("body_1_titre").href="https://youtube.com/shorts/EjUnEEfTSEY?si=UXJsLtCDVFB2a-Zr";
+    document.getElementById("body_1_titre").innerHTML="Drag curl";
+    document.getElementById("body_1_titre").href="https://youtu.be/Nxd9QMbILyo";
     document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
     document.getElementById("body_1_serie").innerHTML="4 séries";
     document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
     document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
     document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
-    document.getElementById("body_1_%MVC").innerHTML="80% MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_%MVC").innerHTML="82% MVC(contraction maximale volontaire)";
 
     
 }if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/T%C3%AAte_courte_du_biceps_brachial/") {
@@ -20,14 +20,14 @@ if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/T%C3%AAte_lon
     document.getElementById("body_0").style.top="-100%";
     document.getElementById("body_1").style.top="0%";
 
-    document.getElementById("body_1_titre").innerHTML="Curl concentration";
-    document.getElementById("body_1_titre").href="https://youtube.com/shorts/EjUnEEfTSEY?si=UXJsLtCDVFB2a-Zr";
+    document.getElementById("body_1_titre").innerHTML="curl barre prise large";
+    document.getElementById("body_1_titre").href="https://youtu.be/JA-4H3moG50";
     document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
     document.getElementById("body_1_serie").innerHTML="4 séries";
     document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
     document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
     document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
-    document.getElementById("body_1_%MVC").innerHTML="85% MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_%MVC").innerHTML="82% MVC(contraction maximale volontaire)";
 
     
 }if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/Coracobrachialis/") {
@@ -51,7 +51,7 @@ if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/T%C3%AAte_lon
     document.getElementById("body_1").style.top="0%";
 
     document.getElementById("body_1_titre").innerHTML="Reverse curl";
-    document.getElementById("body_1_titre").href="https://www.youtube.com/shorts/Oi3bW9nQmGI";
+    document.getElementById("body_1_titre").href="https://www.youtube.com/watch?v=kPXvDlFNb3s&t";
     document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
     document.getElementById("body_1_serie").innerHTML="4 séries";
     document.getElementById("body_1_repos").innerHTML="1min30 de repos par série";
