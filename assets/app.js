82ddd009875a1bb5a96c6b929bb7137526189949
Zehrion <timothefortier64@gmail.com>
2026-03-10 12:04:35 +0100

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,62 @@
 
+
+if (document.body.baseURI == "http://127.0.0.1:5500/3/objectif%202/?/T%C3%AAte_longue_du_biceps_brachial/") {
+    
+    document.getElementById("body_0").style.top="-100%";
+    document.getElementById("body_1").style.top="0%";
+
+    document.getElementById("body_1_titre").innerHTML="Curl concentration";
+    document.getElementById("body_1_titre").href="https://youtube.com/shorts/EjUnEEfTSEY?si=UXJsLtCDVFB2a-Zr";
+    document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
+    document.getElementById("body_1_serie").innerHTML="4 séries";
+    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="80% MVC(contraction maximale volontaire)";
+
+    
+}if (document.body.baseURI == "http://127.0.0.1:5500/3/objectif%202/?/T%C3%AAte_courte_du_biceps_brachial/") {
+    
+    document.getElementById("body_0").style.top="-100%";
+    document.getElementById("body_1").style.top="0%";
+
+    document.getElementById("body_1_titre").innerHTML="Curl concentration";
+    document.getElementById("body_1_titre").href="https://youtube.com/shorts/EjUnEEfTSEY?si=UXJsLtCDVFB2a-Zr";
+    document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
+    document.getElementById("body_1_serie").innerHTML="4 séries";
+    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="85% MVC(contraction maximale volontaire)";
+
+    
+}if (document.body.baseURI == "http://127.0.0.1:5500/3/objectif%202/?/Coracobrachialis/") {
+    
+    document.getElementById("body_0").style.top="-100%";
+    document.getElementById("body_1").style.top="0%";
+
+    document.getElementById("body_1_titre").innerHTML="Tractions prise serrée (chin-up)";
+    document.getElementById("body_1_titre").href="https://www.youtube.com/shorts/Oi3bW9nQmGI";
+    document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
+    document.getElementById("body_1_serie").innerHTML="4 séries";
+    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="70% MVC(contraction maximale volontaire)";
+
+    
+}if (document.body.baseURI == "http://127.0.0.1:5500/3/objectif%202/?/Brachial/") {
+    
+    document.getElementById("body_0").style.top="-100%";
+    document.getElementById("body_1").style.top="0%";
+
+    document.getElementById("body_1_titre").innerHTML="Tractions prise serrée (chin-up)";
+    document.getElementById("body_1_titre").href="https://www.youtube.com/shorts/Oi3bW9nQmGI";
+    document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
+    document.getElementById("body_1_serie").innerHTML="4 séries";
+    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="90 % MVC(contraction maximale volontaire)";
+    
+}
