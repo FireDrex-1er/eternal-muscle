3cdeb0680b7f7de417cd0b605fffa081b08e5bc1
Zehrion <timothefortier64@gmail.com>
2026-03-12 12:41:47 +0100

    illustration update p.2
    illustration update p.2

--- assets/app.js
+++ assets/app.js
@@ -13,6 +13,28 @@ if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/Biceps_brachi
     document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
     document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
     document.getElementById("body_1_%MVC").innerHTML="82% MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_img").href="./?/img_1/";
+
+    
+}if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/img_1/") {
+    
+    document.getElementById("body_0").style.top="-100%";
+    document.getElementById("body_1").style.top="0%";
+    document.getElementById("body_2").style.top="0%";
+
+    document.getElementById("body_1_titre").innerHTML="Drag curl";
+    document.getElementById("body_1_titre").href="https://youtu.be/Nxd9QMbILyo";
+    document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
+    document.getElementById("body_1_serie").innerHTML="4 séries";
+    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="82% MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_img").href="./?/img_1/";
+
+    document.body.children["body_2"].children["img_1"].src="https://i.postimg.cc/PJmFQSTT/Capture_d_écran_2026_03_12_110539.png";
+    document.body.children["body_2"].children["img_2"].src="https://i.postimg.cc/x8KF36n9/Capture_d_écran_2026_03_12_110557.png";
+    document.body.children["body_2"].children[2].href="./?/Biceps_brachii(long_head)/";
 
     
 }if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/Biceps_brachii(short_head)/") {
@@ -28,6 +50,28 @@ if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/Biceps_brachi
     document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
     document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
     document.getElementById("body_1_%MVC").innerHTML="82% MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_img").href="./?/img_2/";
+
+    
+}if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/img_2/") {
+    
+    document.getElementById("body_0").style.top="-100%";
+    document.getElementById("body_1").style.top="0%";
+    document.getElementById("body_2").style.top="0%";
+
+    document.getElementById("body_1_titre").innerHTML="curl barre prise large";
+    document.getElementById("body_1_titre").href="https://youtu.be/JA-4H3moG50";
+    document.getElementById("body_1_reps").innerHTML="12 répéition de l'exercice";
+    document.getElementById("body_1_serie").innerHTML="4 séries";
+    document.getElementById("body_1_repos").innerHTML="1min15 de repos par série";
+    document.getElementById("body_1_tension").innerHTML="Montée : 1–2 secondes<br>Contraction en haut : 2 secondes<br>Descente : 3 secondes";
+    document.getElementById("body_1_poids").innerHTML="65 % du 1RM (charge maximale pour une répétition)";
+    document.getElementById("body_1_%MVC").innerHTML="82% MVC(contraction maximale volontaire)";
+    document.getElementById("body_1_img").href="./?/img_2/";
+
+    document.body.children["body_2"].children["img_1"].src="https://i.postimg.cc/PJmFQSTT/Capture_d_écran_2026_03_12_110539.png";
+    document.body.children["body_2"].children["img_2"].src="https://i.postimg.cc/kGxhvTqM/Capture_d_écran_2026_03_12_112233.png";
+    document.body.children["body_2"].children[2].href="./?/Biceps_brachii(short_head)/";
 
     
 }if (document.body.baseURI == "https://sportif-shadow.netlify.app/?/Brachialis/") {
@@ -116,5 +160,20 @@ if (document.body.offsetWidth <= 1000) {
     document.getElementById("body_1").style.width="100%";
     document.getElementById("body_1").style.backgroundColor="#ddd";
     document.documentElement.style.setProperty('--SizeText1', '17px');
+
+}
+if (document.body.offsetWidth <= 1408) {
+    document.body.children["body_2"].style.height="200%";
+    document.body.children["body_2"].children["img_1"].style.left="50%";
+    document.body.children["body_2"].children["img_1"].style.top="25%";
+    document.body.children["body_2"].children["img_1"].style.transform="translate(-50%,-50%)";
+    document.body.children["body_2"].children["img_1"].style.height="45%";
+    
+    document.body.children["body_2"].children["img_2"].style.left="50%";
+    document.body.children["body_2"].children["img_2"].style.top="75%";
+    document.body.children["body_2"].children["img_2"].style.transform="translate(-50%,-50%)";
+    document.body.children["body_2"].children["img_2"].style.height="45%";
+
+    document.body.children["body_2"].children[2].style.padding="0.5% 3.5% 0.5% 3.5%";
 
 }
